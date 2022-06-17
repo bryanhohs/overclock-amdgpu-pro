339112c85e514c5604a1fc3d61252d6764fbbb8e
Bryan Hohs <bryan@hohs.us>
2022-06-16 23:37:46 -0500

    Update overclock-amdgpu-pro.sh

--- overclock-amdgpu-pro.sh
+++ overclock-amdgpu-pro.sh
@@ -12,9 +12,7 @@ cclock="1209"      # core clocks
 cvddc="850"        # core voltage 
 mclock="2200"      # memory clocks
 mvddc="900"        # memory voltage
-fans="204"         # static fan percentage
-mwatts="100000000" # max wattage per gpu
-rxboost="1"        # rxboost via amdmemtweak
+fans="255"         # static fan percentage
 
 ##################################DO NOT EDIT###################################
 
@@ -32,33 +30,16 @@ done
 
 for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 1 $cclock $cvddc" > $i;
-done
-
-for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 2 $cclock $cvddc" > $i;
-done
-
-for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 3 $cclock $cvddc" > $i;
-done
-
-for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 4 $cclock $cvddc" > $i;
-done
-
-for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 5 $cclock $cvddc" > $i;
-done
-
-for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 6 $cclock $cvddc" > $i;
-done
-
-for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
 	echo "s 7 $cclock $cvddc" > $i;
 done
 
 for i in /sys/class/drm/card*/device/pp_od_clk_voltage; do
+	echo "m 1 $mclock $mvddc" > $i;
 	echo "m 2 $mclock $mvddc" > $i;
 done
 
@@ -74,10 +55,6 @@ for i in /sys/class/drm/card*/device/pp_dpm_sclk; do
         echo 6 > $i;
 done
 
-for i in /sys/class/drm/card*/device/hwmon/hwmon*/power1_cap; do
-	echo $mwatts > $i;
-done
-
-if [[ $rxboost == 1 ]]; then amdmemtweak --REF 30 > /dev/null 2>&1; fi 
+amdmemtweak --REF 30 > /dev/null 2>&1;
 
 exit 0
